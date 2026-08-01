c9e5403c91874539f0132136cde1798b6fa68da3
emkas77-dev <emkas77@gmail.com>
2026-08-01 11:14:32 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import OperationsRoom from './OperationsRoom-11.jsx'
+import Deepseek_jsx_20260801_6939c6.jsx
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
