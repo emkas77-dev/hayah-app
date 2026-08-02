5318c40e865afd07a449ede6e6d8df1ff16f3106
emkas77-dev <emkas77@gmail.com>
2026-08-02 09:28:37 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import OperationsRoom-19.jsx
+import OperationsRoom-11.jsx
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
