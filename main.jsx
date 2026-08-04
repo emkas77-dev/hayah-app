51944dde208189594acaee5a3b05576290c5666f
emkas77-dev <emkas77@gmail.com>
2026-08-04 10:35:46 +0200

    Rename import from OperationsRoom-11 to OperationsRoom

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import OperationsRoom-11.jsx
+import OperationsRoom.jsx
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
