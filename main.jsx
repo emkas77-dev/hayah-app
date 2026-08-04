3cde0c1b548afc6a0fdbaa37d1f82e46af6f574e
emkas77-dev <emkas77@gmail.com>
2026-08-04 11:26:29 +0200

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import OperationsRoom from OperationsRoom-18.jsk
+import OperationsRoom from './OperationsRoom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
