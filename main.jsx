31f18520846987d72cfbfd3dead97aae4590add7
emkas77-dev <emkas77@gmail.com>
2026-08-08 22:40:26 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import OperationsRoom from './OperationsRoom'
-
+import OperationsRoom from './OperationsRoom-20.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <OperationsRoom />
