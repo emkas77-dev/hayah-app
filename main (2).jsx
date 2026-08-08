4199c244083e6cbc00ffe3b8e666481c62613ef5
emkas77-dev <emkas77@gmail.com>
2026-08-08 15:27:01 +0300

    Delete main (2).jsx

--- main (2).jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import OperationsRoom from './OperationsRoom-20.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <OperationsRoom />
-  </React.StrictMode>
-)
