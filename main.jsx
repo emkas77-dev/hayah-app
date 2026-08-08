98afa2edd466e3530c8700e84bc616f994a52b0f
emkas77-dev <emkas77@gmail.com>
2026-08-08 21:37:37 +0200

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,1 +1,9 @@
-import OperationsRoom from './OperationsRoom-20.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OperationsRoom from './OperationsRoom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OperationsRoom />
+  </React.StrictMode>
+)
