6748869fef0a30c05849796373dc9a9f31317f9a
emkas77-dev <emkas77@gmail.com>
2026-08-08 15:19:18 +0300

    Update import for OperationsRoom component

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import OperationsRoom from './OperationsRoom(15).jsx
+import OperationsRoom from './OperationsRoom-20.jsx
 ReactDOM.createRoot(document.getElementById('root')).render(
   <OperationsRoom />
 )
