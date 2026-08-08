77781507bfc159d0a2aa4d79475435f6cd07120c
emkas77-dev <emkas77@gmail.com>
2026-08-08 22:23:20 +0300

    Fix import statement for OperationsRoom component

--- main.jsx
+++ main.jsx
@@ -1,7 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import OperationsRoom from './OperationsRoom-20.jsx
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <OperationsRoom />
-)
+import OperationsRoom from './OperationsRoom-20.jsx'
