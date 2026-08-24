fbfe07a495d8194cad9fa4ecaf5b3766a637b7e7
emkas77-dev <emkas77@gmail.com>
2026-08-24 17:23:38 +0300

    Update import for OperationsRoom component

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import OperationsRoom from './OperationsRoom-28.jsx'
+import OperationsRoom from './OperationsRoom-38.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <OperationsRoom />
